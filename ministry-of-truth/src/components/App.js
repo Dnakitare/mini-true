1d8172a0539b8f42a29dad9ec5f2336a6f0c76df
Daniel Nakitare <dnakitare@cnm.edu>
2019-01-19 11:20:00 -0700

    splash element with styling

---
+++ ministry-of-truth/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="mini-true">
+        <header>
+			  <h1>[Ministry-of-Truth]</h1>
+			  <p>"Freedom of thought makes slaves of us all"<br/>- Big Brother</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ministry-of-truth/src/components/Router.js
@@ -0,0 +1,3 @@
+import  React from 'react';
+import {BrowserROuter, Route, Switch} from 'react-router-dom';
+import App from './App';
